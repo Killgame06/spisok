00e60510b89aab6ac0e7bcd5d2b7a23efe95d0ef
Killgame <ilgamkildiarov@yandex.ru>
2022-11-05 22:30:14 +0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+        val list = listOf(1,2,3,4,5,6,7,8,9,10)
+        var sum= 7
+        for (element in list){
+            sum += element
+        }
+        println(list)
+
+    }
